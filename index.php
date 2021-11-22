c8c39b940777f8c62a747040d211eb74ffd3256d
zerodark10 <61922388+zerodark10@users.noreply.github.com>
2021-11-22 12:36:51 -0600

    z

--- index.php
+++ index.php
@@ -1,38 +1,4 @@
 
-    <?php
-     include("php/base.php");
-   
-     session_start();
-     if(isset ($_SESSION['id_usuario'])){
-       header("Location: home.php");
-     }
-     //registrar
-     if (!empty ($_POST)){
-        
-        $correo =mysqli_real_escape_string($conn,$_POST['email']);
-        $password =mysqli_real_escape_string($conn,$_POST['password']);
-    
-
-        $sql = "SELECT email FROM users WHERE email ='$correo' AND password ='$password' ";
-        
-        $resultado =$conn->query($sql);
-        $rows =$resultado->num_rows;
-        if($rows > 0){
-          $row =$resultado->fetch_assoc();
-          $_SESSION['iduser']=$row["email"];
-          header("Location: home.php");
-         
-        }else{
-          echo"<script>
-          alert('usuario o password incorrecto');
-          windows.location='index.php';
-          </script>";
-          }
-        
-     }
-
-
-    ?>
     <!DOCTYPE html>
 <html lang="en">
   <head>
