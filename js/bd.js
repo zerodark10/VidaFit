d82586d0dd6fc2f06c2dc490487e120f5a88ac26
zerodark10 <61922388+zerodark10@users.noreply.github.com>
2021-11-26 11:38:28 -0600

    Update bd.js

--- js/bd.js
+++ js/bd.js
@@ -1,6 +1,6 @@
 
 
-
+function db (){};
 var db=openDatabase('Vidafit', '1.0', 'usuarios', 2 * 1024 * 1024);
 db.transaction(function (tx) {
     tx.executeSql('CREATE TABLE IF NOT EXISTS tweets(id, email, apellidos,email,contrasena )', [], getTweets);
